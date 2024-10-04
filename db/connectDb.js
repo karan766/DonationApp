432a0d6578c2885b576e6ec19cf1002fe7717a3e
Your Name <you@example.com>
2024-10-04 10:36:58 +0530

    -m "done"

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,7 +2,7 @@
 
  const connectDB= async()=>{
     try{
-        const conn = await mongoose.connect(`mongodb://127.0.0.1:27017/collection-project`
+        const conn = await mongoose.connect(`mongodb+srv://karansodhi703:PcUhK4LmUC6DTtjO@cluster0.bvaylqi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
         );
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     }catch(err){
